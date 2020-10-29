b0c9a47d509f8cdcb6da800dfe4540c6566d1d00
Apoorve Singhal <sapoorve@gmail.com>
2020-10-30 01:14:17 +0530

    plotting percentage confidence text on image;

--- slit/bb.py
+++ slit/bb.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import cv2
 
 class BoundingBox():
 
@@ -15,7 +16,7 @@ class BoundingBox():
         )
         self.confidence_th = st.slider(
             label='Only objects with >= than this threshold will be shown', 
-            min_value=0.0, max_value=1.0, value=0.0, step=0.05
+            min_value=0.0, max_value=1.0, value=0.0, step=0.01
         )
 
         self.colors = {
@@ -48,6 +49,11 @@ class BoundingBox():
 
                     image_with_boxes[int(ymin*height):int(ymax*height),int(xmin*width):int(xmax*width),:] += self.colors[cat]
                     image_with_boxes[int(ymin*height):int(ymax*height),int(xmin*width):int(xmax*width),:] /= 2
+                    cv2.putText(
+                        img=image_with_boxes, text=f'{confidence*100}%', org=(int(xmin*width),int(ymin*height)), color=self.colors[cat],
+                        fontFace=0, fontScale=1.3, thickness=2
+                    )
+                
 
         st.image(
             image_with_boxes.astype(np.uint8), use_column_width=True,
