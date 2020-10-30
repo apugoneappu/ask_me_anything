d768435cef9eceb76ad878170646ec8581a20e76
Apoorve Singhal <sapoorve@gmail.com>
2020-10-30 19:15:33 +0530

    changed colour schemes, refactored ax2im function for reuse

--- vqa/visualisations/vis.py
+++ vqa/visualisations/vis.py
@@ -8,14 +8,26 @@ import streamlit as st
 import os
 from PIL import Image, ImageDraw
 
-def hbarplot(x, y, data):
-
-    ax = sns.barplot(x=x, y=y, data=data, orient='h')
-
+def ax2im(ax):
     buf = io.BytesIO()
-    plt.savefig(buf, format='png')
+    plt.savefig(buf, format='jpg', bbox_inches = 'tight', pad_inches = 0)
     buf.seek(0)
     im = Image.open(buf)
+    return im
+
+def hbarplot(x, y, data, **kwargs):
+
+    plt.clf()
+    ax = sns.barplot(x=x, y=y, data=data, orient='h', palette="Blues_d", **kwargs)
+    im = ax2im(ax)
+    
+    return im
+
+def heatmap(data, annot, **kwargs):
+
+    ax = sns.heatmap(data=data, annot=annot, fmt="s", cmap='rocket', cbar=True, square=True, xticklabels=False, yticklabels=False, **kwargs)
+    im = ax2im(ax)
+    
     return im
 
 def show_architecture(model_name):
