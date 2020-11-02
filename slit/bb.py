3824c4f3000f3194dfc5343fdeec1534b3c2f7f6
Apoorve Singhal <sapoorve@gmail.com>
2020-11-02 12:59:21 +0530

    removed show_all, added show_color buttons;

--- slit/bb.py
+++ slit/bb.py
@@ -21,21 +21,40 @@ class BoundingBox():
             with attention score between 5% to 10% and the red boxes with less than 5%.'
         )
 
+        self.cols = st.beta_columns(bboxes.shape[0])
+        self.button_cols = st.beta_columns(3)
+
         self.image = np.copy(image)
         self.bboxes = bboxes
 
-        self.show_all = st.checkbox(
-            label='Show/Hide all objects', value=True, key='show_all'
+        with self.button_cols[0]:
+            self.show_green = st.checkbox(
+                label='Show green objects', value=True, key='show_green'
         )
+        with self.button_cols[1]:
+            self.show_blue = st.checkbox(
+                label='Show blue objects', value=True, key='show_blue'
+        )
+        with self.button_cols[2]:
+            self.show_red = st.checkbox(
+                label='Show red objects', value=False, key='show_red'
+        )
+
         self.confidence_th = st.slider(
             label='Only objects with >= than this threshold will be shown', 
             min_value=0.0, max_value=1.0, value=0.07, step=0.01
         )
 
         self.colors = {
-            "max": [0, 255, 0],
-            "mid": [0, 0, 170],
-            "min": [100, 0, 0]
+            "green": [0, 255, 0],
+            "blue": [0, 0, 170],
+            "red": [100, 0, 0]
+        }
+
+        self.is_shown = {
+            "green": self.show_green,
+            "blue": self.show_blue,
+            "red": self.show_red
         }
 
         self.plot_boxes()
@@ -47,18 +66,21 @@ class BoundingBox():
         
         height, width, channels = image_with_boxes.shape
 
-        if self.show_all:
+        for idx in range(self.bboxes.shape[0]):
 
-            for (xmin, ymin, xmax, ymax, confidence) in self.bboxes:
+            for (xmin, ymin, xmax, ymax, confidence) in self.bboxes[idx]:
 
                 if (confidence >= self.confidence_th):
 
                     if (confidence <= 0.05):
-                        cat = "min"
+                        cat = "red"
                     elif (0.05 < confidence <= 0.1):
-                        cat = "mid"
+                        cat = "blue"
                     else:
-                        cat = "max"
+                        cat = "green"
+
+                    if (not self.is_shown[cat]):
+                        continue
 
                     image_with_boxes[int(ymin*height):int(ymax*height),int(xmin*width):int(xmax*width),:] += self.colors[cat]
                     image_with_boxes[int(ymin*height):int(ymax*height),int(xmin*width):int(xmax*width),:] /= 2
@@ -66,12 +88,12 @@ class BoundingBox():
                         img=image_with_boxes, text=f'{confidence*100:.2f}%', org=(int(xmin*width),int(ymin*height)), color=(0,0,0),
                         fontFace=0, fontScale=0.8, thickness=2
                     )
-                
-
-        st.image(
-            image_with_boxes.astype(np.uint8), use_column_width=True,
-            caption='Image with bounding boxes (Green > 10%, 5% < blue < 10%, red < 5%)'
-        )
+                    
+            with self.cols[idx]:
+                st.image(
+                    image_with_boxes.astype(np.uint8), use_column_width=True,
+                    caption=f'Prediction #{idx}'
+                )
             
         
 
