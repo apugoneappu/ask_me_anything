49e0d81dbfcf251b3f99088fbfffd18618341617
Apoorve Singhal <sapoorve@gmail.com>
2020-11-06 15:33:18 +0530

    v1 working

--- vqa/vqa.py
+++ vqa/vqa.py
@@ -14,14 +14,14 @@ import gdown
 @st.cache
 class VQA():
 
-    def __init__(self, long_model_name):
+    def __init__(self, model_name):
         """
         Args:
-            long_model_name (str): The string selected from the dropdown menu in streamlit
+            model_name (str): The corrected model string ('mfb', 'mcan', etc)
         """
 
         # such as 'mfb', 'mcan' etc.
-        self.model_name = self.get_model_name(long_model_name)
+        self.model_name = model_name
 
         with open(f'vqa/configs/{self.model_name}.yml', 'r') as f:
             self.config = yaml.load(f, Loader=yaml.FullLoader)
@@ -35,17 +35,6 @@ class VQA():
         self.token_to_ix = dataset['token_to_ix']
         self.ix_to_answer = dataset['ix_to_ans']
 
-    def get_model_name(self, long_model_name):
-
-        if ('MFB' in long_model_name):
-            return 'mfb'
-        
-        elif ('MCAN' in long_model_name):
-            return 'mcan'
-        
-        else:
-            raise NotImplementedError
-
     def get_net(self, model_name):
 
         url = 'https://drive.google.com/uc?id='
@@ -53,10 +42,8 @@ class VQA():
 
         if (model_name == 'mfb'):
 
-            ################## #TODO hardcode token_size ###################
             net = Net_MFB(self.config, token_size=20573, answer_size=3129)
             net.eval()
-            ###############################################################
 
             url += '1lHTuq_wfIMVxZDPvCuq3nFVmnF1ohkzb'
             output += 'mfb.pkl'
@@ -67,11 +54,11 @@ class VQA():
         if (not os.path.isfile(f'./vqa/pickles/{model_name}.pkl')):
             gdown.download(url, output, quiet=False)
 
-        net.load_state_dict(torch.load(f'./vqa/pickles/{model_name}.pkl', map_location='cpu')['state_dict'], strict=False)
+        net.load_state_dict(torch.load(f'./vqa/pickles/{model_name}.pkl', map_location='cpu')['state_dict'], strict=True)
         
         return net
     
-    def inference(self, question, image):
+    def inference(self, question, image_feat):
         """Function used for running inverence given the image and question
 
         Args:
@@ -93,12 +80,14 @@ class VQA():
         ques_ix = proc_ques(question, self.token_to_ix, max_token=14)
         ques_ix = torch.tensor(ques_ix).unsqueeze(0)
 
-        # extract features from image
-        ## Plugging in random value for now ##
-        frcn_feat = torch.ones(1, 100, 2048)
-        frcn_feat_mask = make_mask(frcn_feat)
+        # pad image features
+        frcn_feat = torch.zeros(100, 2048)
+        frcn_feat[:image_feat.shape[0]] = image_feat
+        image_feat = frcn_feat.unsqueeze(0)
 
-        ret = self.net.forward(frcn_feat, frcn_feat_mask, ques_ix)
+        image_feat_mask = make_mask(image_feat)
+
+        ret = self.net.forward(image_feat, image_feat_mask, ques_ix)
 
         return ret
 
