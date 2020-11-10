953771c09734207f823d5004a44ed778ee5c6210
Apoorve Singhal <sapoorve@gmail.com>
2020-11-10 10:19:00 +0530

    script for images rename/copy

--- utils/rename.py
+++ utils/rename.py
@@ -1,22 +1,34 @@
 import os
+from shutil import copyfile
 
-cwd = os.getcwd()
-# print(cwd)
-for idx, f in enumerate(os.listdir('./images/')):
-    
-    img_path = os.path.join(cwd, 'images', f)
-    new_img_path = os.path.join(cwd, 'images', str(idx)+'.jpg')
-    
-    os.rename(img_path, new_img_path)
+root_dir = '/Users/apoorve/Downloads/my'
 
+dst_dir = '/Users/apoorve/Desktop/Personal/ama/assets'
+
+src_images_dir = os.path.join(root_dir, 'images')
+src_feat_dir = os.path.join(root_dir, 'feats')
+
+dst_images_dir = os.path.join(dst_dir, 'images')
+dst_feat_dir = os.path.join(dst_dir, 'feats')
+# print(root_dir)
+
+for idx, f in enumerate(os.listdir(src_images_dir)):
+    
+    img_path = os.path.join(src_images_dir, f)
+    new_img_path = os.path.join(dst_images_dir, str(33+idx)+'.jpg')
+
+    feat_path = os.path.join(src_feat_dir, f.split('.')[0] + '.npz')
+    new_feat_path = os.path.join(dst_feat_dir, str(33+idx)+'.npz')
+
+    if (os.path.isfile(img_path) and os.path.isfile(feat_path)):
+        copyfile(img_path, new_img_path)
+        copyfile(feat_path, new_feat_path)
+        # os.rename(img_path, new_img_path)
+        # os.rename(feat_path, new_feat_path)
+    
     if (not os.path.isfile(img_path)):
         print(img_path)
-
-    feat_path = os.path.join(cwd, 'feats', f+'.npz')
-    new_feat_path = os.path.join(cwd, 'feats', str(idx)+'.npz')
-
 
     if (not os.path.isfile(feat_path)):
         print(feat_path)
 
-    os.rename(feat_path, new_feat_path)
